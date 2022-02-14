80790c46ed30dfca8305e50d9931d5b6eb6d5842
Graham Bygrave <graham@bygrave.org>
2022-02-14 11:34:50 +0000

    Fixed

--- DynamoDBScan.java
+++ DynamoDBScan.java
@@ -10,36 +10,36 @@ import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
 /**
  * Return all items in a DynamoDB Table
  * <p>
  * This code expects that you have AWS credentials set up per:
  * http://docs.aws.amazon.com/java-sdk/latest/developer-guide/setup-credentials.html
  */
-public class DynamoDBScan implements RequestHandler<Map<String,String>, String> {
+public class DynamoDBScan implements RequestHandler<Map<String, String>, String> {
 
-    static String tableName = "<FMI1>";
+	static String tableName = "<FMI1>";
+	private static final AmazonDynamoDB ADB_CLIENT = AmazonDynamoDBClientBuilder.standard().withRegion("eu-west-2").build();
+	private static final DynamoDB DYNAMO_DB = new DynamoDB(ADB_CLIENT);
 
-    @Override
-    public String handleRequest(Map<String,String> event, Context context)
-    {
-        return findAllItems();
-    }
+	@Override
+	public String handleRequest(Map<String, String> event, Context context) {
+		return findAllItems();
+	}
 
-    private static String findAllItems() {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-        DynamoDB dynamoDB = new DynamoDB(client);
-        Table table = dynamoDB.getTable(tableName);
+	private static String findAllItems() {
+		Table table = DYNAMO_DB.getTable(tableName);
 
-        ItemCollection<ScanOutcome> items = table.scan();
+		ItemCollection<ScanOutcome> items = table.scan();
 
-        Iterator<Item> iterator = items.iterator();
-        String itemsToReturn = "";
-        while (iterator.hasNext()) {
-            itemsToReturn += iterator.next().toJSONPretty();
-        }
+		Iterator<Item> iterator = items.iterator();
+		String itemsToReturn = "";
+		while (iterator.hasNext()) {
+			itemsToReturn += iterator.next().toJSONPretty();
+		}
 
-        return itemsToReturn;
-    }
+		return itemsToReturn;
+	}
 
 }
