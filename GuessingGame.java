6b72a308806d7375c666927f6e1ab66ac618d6e6
Graham Bygrave <graham@bygrave.org>
2022-02-14 10:29:50 +0000

    blah

--- GuessingGame.java
+++ GuessingGame.java
@@ -8,66 +8,27 @@ public class GuessingGame {
         System.out.println("Enter a guess between 1 and 100");
         int userAnswer = scanner.nextInt();
 
-        // Guess 1
-        if (userAnswer <=0 || userAnswer >100) {
-            System.out.println("Invalid response");
+        for (int tries=0; tries<3; tries++) {
+        
+	        // Guess 'tries'
+	        if (userAnswer <=0 || userAnswer >100) {
+	            System.out.println("Invalid response");
+	        }
+	        else if (userAnswer == computerNumber ){
+	            System.out.println("Correct! You win! See you next time!");
+	            System.exit(0);
+	        }
+	        else if (userAnswer > computerNumber) {
+	            System.out.println("Your guess is too high, guess again.");
+	        }
+	        else if (userAnswer < computerNumber) {
+	            System.out.println("Your guess is too low, guess again.");
+	        }
+	        else {
+	            System.out.println("Your guess is incorrect");
+	        }
         }
-        else if (userAnswer == computerNumber ){
-            System.out.println("Correct! You win! See you next time!");
-            System.exit(0);
-        }
-        else if (userAnswer > computerNumber) {
-            System.out.println("Your guess is too high, guess again.");
-        }
-        else if (userAnswer < computerNumber) {
-            System.out.println("Your guess is too low, guess again.");
-        }
-        else {
-            System.out.println("Your guess is incorrect");
-        }
-
-        // Guess 2
-        System.out.println("Enter a guess between 1 and 100");
-        userAnswer = scanner.nextInt();
-
-        if (userAnswer <=0 || userAnswer >100) {
-            System.out.println("Invalid response");
-        }
-        else if (userAnswer == computerNumber ){
-            System.out.println("Correct! You win! See you next time!");
-            System.exit(0);
-        }
-        else if (userAnswer > computerNumber) {
-            System.out.println("Your guess is too high, guess again.");
-        }
-        else if (userAnswer < computerNumber) {
-            System.out.println("Your guess is too low, guess again.");
-        }
-        else {
-            System.out.println("Your guess is incorrect");
-        }
-
-        // Guess 3
-        System.out.println("Enter a guess between 1 and 100");
-        userAnswer = scanner.nextInt();
-
-        if (userAnswer <=0 || userAnswer >100) {
-            System.out.println("Invalid response");
-        }
-        else if (userAnswer == computerNumber ){
-            System.out.println("Correct! You win! See you next time!");
-            System.exit(0);
-        }
-        else if (userAnswer > computerNumber) {
-            System.out.println("Your guess is too high");
-        }
-        else if (userAnswer < computerNumber) {
-            System.out.println("Your guess is too low, guess again.");
-        }
-        else {
-            System.out.println("Your guess is incorrect");
-        }
-
-        System.out.println("Sorry for your luck, restart the program to play again!");
+        
+	    System.out.println("Sorry for your luck, restart the program to play again!");
     }
 }
